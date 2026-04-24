5c5f679cbc28ff70f461b733831017e43c2fa2a4
Mary Strodl <mary.strodl@bryx.com>
2026-04-24 15:55:23 -0400

    Revert "linux-native: provide better signatures for the ioctl wrappers"
    This reverts commit cc1b2b3b833075c4f9831188062b50357ca80e63.
    
    Kernel definitions say they are read-write, so we get EINVAL since it's unknown:

--- src/linux_native.rs
+++ src/linux_native.rs
@@ -536,7 +536,14 @@ impl HidDeviceBackendBase for HidDevice {
             return Err(HidError::InvalidZeroSizeData);
         }
 
-        let res = match unsafe { hidraw_ioc_set_feature(self.fd.as_raw_fd(), data) } {
+        // Have to crate owned buffer, because its not safe to cast shared
+        // reference to mutable reference, even if the underlying function never
+        // tries to mutate it.
+        let mut d = data.to_vec();
+
+        // The ioctl is marked as read-write so we need to mess with the
+        // mutability even though nothing should get written
+        let res = match unsafe { hidraw_ioc_set_feature(self.fd.as_raw_fd(), &mut d) } {
             Ok(n) => n as usize,
             Err(e) => {
                 return Err(HidError::HidApiError {
